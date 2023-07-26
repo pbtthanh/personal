64a7792db620b3466f8203eeca52dbc39fc91140
thanh98thehepro <16130579@st.hcmuaf.edu.vn>
2023-07-26 15:21:34 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstPageComponent } from './first-page/first-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'first-component', component: FirstPageComponent },
+  { path: '',   redirectTo: '/first-component', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
