ee14403133a1637e4c2ce13dbfd63ccf4d957b29
pbtthanh <139889485+pbtthanh@users.noreply.github.com>
2023-08-07 14:46:28 +0700

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Personal-Website');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Personal-Website app is running!');
-  });
+  
 });
