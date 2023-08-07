8f5c71398cc8675dd665c2157f802483eacc4bcc
pbtthanh <139889485+pbtthanh@users.noreply.github.com>
2023-08-07 16:30:34 +0700

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,12 +19,5 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'Personal-Website'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Personal-Website');
-  });
-
   
 });
